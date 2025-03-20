033dea90bc608c798a223191f876eae3e6542a8a
Luis Enrique Flores <32490628+luisenriqueflores@users.noreply.github.com>
2025-03-19 23:02:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,7 @@ async def extract_name_from_id(upload: UploadFile = File(...)):
         return {"Data": alpr_results}
     except Exception as e:
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=2000)
